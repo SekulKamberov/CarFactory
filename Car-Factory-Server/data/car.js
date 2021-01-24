17954e3b2ed80942fe28a166a15c105d3cda7951
Sekul Kamberov <microfixbg@gmail.com>
2021-01-24 14:39:49 +0200

    .
    .

---
+++ Car-Factory-Server/data/car.js
@@ -0,0 +1,15 @@
+const carData = {}
+let currentId = 0
+
+module.exports = {
+    total: 
+    save:
+    all:
+    findById:
+    addReview:
+    allReviews:
+    like:
+    byUser:
+    delete:
+    edit:
+}
