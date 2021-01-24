7685794587d6efec273ea63148f2d892cbc56d9c
Sekul Kamberov <microfixbg@gmail.com>
2021-01-24 15:58:59 +0200

    create routes/car.js not implemented
    create routes/car.js not implemented

--- Car-Factory-Server/routes/car.js
+++ Car-Factory-Server/routes/car.js
@@ -1,2 +1,23 @@
 const express = require('express')
 const authCheck = require('../middleware/auth-check')
+const carData = require('..data/car')
+
+const router = new express.Router()
+
+function validateCar () {
+
+}
+
+router.post()
+
+router.get()
+
+router.get()
+
+router.delete()
+
+router.put()
+
+router.edit()
+
+module.exports = router
