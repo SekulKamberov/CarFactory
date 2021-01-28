30b2f9d2494dbe57c3c8b04069aaa42c4c71fba6
Sekul Kamberov <microfixbg@gmail.com>
2021-01-28 14:06:44 +0200

    car & create-car models ready
    folder models with
    car.model.ts
    create-car.model.ts
    is created

---
+++ CarFactory-Client/src/app/car/models/car.model.ts
@@ -0,0 +1,14 @@
+export class CarModel {
+    public id: string
+    public make: string
+    public model: string
+    public engine: string
+    public hp: number
+    public doors: number
+    public gearbox: string
+    public year: number
+    public description: string
+    public price: number
+    public image: string
+    public createdBy: string
+}
