fece779f3cdcb0f5992c17fb21b253d0282a43ee
Sekul Kamberov <microfixbg@gmail.com>
2021-01-28 14:17:38 +0200

    car model modified
    car model modified

--- CarFactory-Client/src/app/car/car.service.ts
+++ CarFactory-Client/src/app/car/car.service.ts
@@ -1,10 +1,22 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { CarModel } from './models/car.model'
+import { CreateCarModel } from './models/create-car.model'
+
+const createUrl = 'http://localhost:5000/car/create'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createCar (model: CreateCarModel) {
+    return this.http.post(createUrl, model)
+  }
+
+
+
 }
